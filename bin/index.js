e07ad4b16b8eac658a63918ebfeccf795af69a84
Caleb <calebjrbako1231@gmail.com>
2025-10-22 22:54:27 +0100

    Additions

--- bin/index.js
+++ bin/index.js
@@ -2,9 +2,16 @@
 import inquirer from "inquirer";
 import path from "path";
 import {
+  createFrontendfolderFiles,
+  createFrontendMainFolders,
+  createFSBackendFolders,
+  createFSFrontendFolders,
+  createFSVFrontendFolders,
   createMainFolders,
   createParentFolder,
   createSubfolderFiles,
+  createVanillafolderFiles,
+  createVanillaFrontendFolders,
 } from "../src/setup.js";
 
 const __dirname = process.cwd();
@@ -17,9 +24,15 @@ const questionsPrompt = [
   },
   {
     type: "list",
+    name: "type",
+    message: "Project Type",
+    choices: ["Frontend", "Backend", "Both"],
+  },
+  {
+    type: "list",
     name: "direction",
-    message: "Which project structure are you using?",
-    choices: ["Forward", "MERN", "VanillaJS", "MEAN"],
+    message: "Which tech stack are you using?",
+    choices: ["MERN", "VanillaJS"],
   },
 ];
 
@@ -27,18 +40,53 @@ function main() {
   createFolders();
 }
 
+//Add folder structure for vanillajs frontend and fullstack
 function createFolders() {
   inquirer.prompt(questionsPrompt).then((answers) => {
     let dirPath = path.join(__dirname, answers.folder);
-    createParentFolder(dirPath).then(() => {
-      if (answers.direction === "VanillaJS") {
-        createMainFolders(dirPath);
-        createSubfolderFiles(dirPath);
-      } else {
-        console.log("Option not available");
-      }
-    });
+ 
+      createParentFolder(dirPath).then(() => {
+        if (
+          answers.type === "Backend" &&
+          (answers.direction === "VanillaJS" ||
+            answers.direction === "MERN")
+        ) {
+          createMainFolders(dirPath);
+          createSubfolderFiles(dirPath);
+           console.log("Done Creating...");
+        } else if (answers.type === "Frontend" && answers.direction === "MERN") {
+          createFrontendMainFolders(dirPath);
+          createFrontendfolderFiles(dirPath);
+           console.log("Done Creating...");
+        } else if (answers.type === "Frontend" && answers.direction === "VanillaJS") {
+          createVanillaFrontendFolders(dirPath);
+          createVanillafolderFiles(dirPath);
+           console.log("Done Creating...");
+        } else if (answers.type === "Both" && answers.direction === "MERN") {
+          createFSFrontendFolders(dirPath);
+          createFSBackendFolders(dirPath);
+           console.log("Done Creating...");
+        }else{
+          createFSVFrontendFolders(dirPath);
+          createFSBackendFolders(dirPath);
+           console.log("Done Creating...");
+        }
+      }); 
   });
 }
+
+
+// function encounter2b() {
+//   inquirer
+//     .prompt({
+//       type: "list",
+//       name: "weapon",
+//       message: "Which project structure are you using?",
+//       choices: ["MERN", "VanillaJS", "MEAN"],
+//     })
+//     .then(() => {
+//       console.log("The wolf mauls you. You die. The end.");
+//     });
+// }
 
 main();
--- src/setup.js
+++ src/setup.js
@@ -1,5 +1,6 @@
 import * as fs from "fs/promises";
 
+//Backend creation
 export async function createMainFolders(dirPath) {
   //creating src folder
   await fs.mkdir(`${dirPath}/src`, { recursive: true });
@@ -13,6 +14,7 @@ export async function createMainFolders(dirPath) {
   await fs.mkdir(`${dirPath}/src/utils`, { recursive: true });
 }
 
+
 export async function createSubfolderFiles(dirPath) {
   //create server file
   await fs.writeFile(`${dirPath}/index.js`, "server");
@@ -24,8 +26,112 @@ export async function createSubfolderFiles(dirPath) {
 
   //create subfolders
   await fs.writeFile(`${dirPath}/src/db/index.js`, "database");
-  console.log("Done Creating...");
 }
+
+//Frontend creation
+
+//MERN
+export async function createFrontendMainFolders(dirPath) {
+  //creating src folder
+  await fs.mkdir(`${dirPath}/public`, { recursive: true });
+  await fs.mkdir(`${dirPath}/src`, { recursive: true });
+
+  //creating files inside src folder
+  await fs.mkdir(`${dirPath}/src/components`, { recursive: true });
+  await fs.mkdir(`${dirPath}/src/pages`, { recursive: true });
+  await fs.mkdir(`${dirPath}/src/assets`, { recursive: true });
+  await fs.mkdir(`${dirPath}/src/services`, { recursive: true });
+  await fs.mkdir(`${dirPath}/src/context`, { recursive: true });
+}
+
+export async function createFrontendfolderFiles(dirPath) {
+  await fs.writeFile(`${dirPath}/index.js`, "server");
+  await fs.writeFile(`${dirPath}/App.js`, "app");
+  await fs.writeFile(`${dirPath}/.gitignore`, "ignore");
+}
+
+
+//VanillaJS
+export async function createVanillaFrontendFolders(dirPath) {
+  //creating src folder
+  await fs.mkdir(`${dirPath}/public`, { recursive: true });
+  await fs.mkdir(`${dirPath}/src`, { recursive: true });
+
+
+  //creating files inside src folder
+  await fs.mkdir(`${dirPath}/public/css`, { recursive: true });
+  await fs.mkdir(`${dirPath}/src/components`, { recursive: true });
+  await fs.mkdir(`${dirPath}/src/modules`, { recursive: true });
+  await fs.mkdir(`${dirPath}/src/utils`, { recursive: true });
+}
+
+export async function createVanillafolderFiles(dirPath) {
+  await fs.writeFile(`${dirPath}/main.js`, "server");
+  await fs.writeFile(`${dirPath}/app.js`, "app");
+  await fs.writeFile(`${dirPath}/.gitignore`, "ignore");
+
+   await fs.mkdir(`${dirPath}/public/index.html`, { recursive: true });
+   await fs.mkdir(`${dirPath}/public/css/style.css`, { recursive: true });
+}
+
+//FullStack
+export async function createFSFrontendFolders(dirPath) {
+  //creating src folder - MERN
+  await fs.mkdir(`${dirPath}/frontend`, { recursive: true });
+
+  await fs.mkdir(`${dirPath}/frontend/src`, { recursive: true });
+  await fs.mkdir(`${dirPath}/frontend/public`, { recursive: true });
+
+  //creating files inside src folder
+ await fs.mkdir(`${dirPath}/frontend/src/components`, { recursive: true });
+ await fs.mkdir(`${dirPath}/frontend/src/pages`, { recursive: true });
+ await fs.mkdir(`${dirPath}/frontend/src/assets`, { recursive: true });
+ await fs.mkdir(`${dirPath}/frontend/src/services`, { recursive: true });
+ await fs.mkdir(`${dirPath}/frontend/src/context`, { recursive: true });
+
+}
+
+export async function createFSVFrontendFolders(dirPath) {
+  //creating src folder - VanillaJS
+  await fs.mkdir(`${dirPath}/client`, { recursive: true });
+  await fs.mkdir(`${dirPath}/client/public`, { recursive: true });
+
+  //creating files inside src folder
+  await fs.mkdir(`${dirPath}/client/components`, { recursive: true });
+  await fs.mkdir(`${dirPath}/client/pages`, { recursive: true });
+  await fs.mkdir(`${dirPath}/client/assets`, { recursive: true });
+  await fs.mkdir(`${dirPath}/client/services`, { recursive: true });
+  await fs.mkdir(`${dirPath}/client/context`, { recursive: true });
+
+   await fs.writeFile(`${dirPath}/client/main.js`, "server");
+   await fs.writeFile(`${dirPath}/client/app.js`, "app");
+   await fs.writeFile(`${dirPath}/client/.gitignore`, "ignore");
+
+   await fs.mkdir(`${dirPath}/client/public/index.html`, { recursive: true });
+   await fs.mkdir(`${dirPath}/client/public/css/style.css`, {
+     recursive: true,
+   });
+
+}
+
+export async function createFSBackendFolders(dirPath) {
+  //creating src folder
+  await fs.mkdir(`${dirPath}/backend`, { recursive: true });
+
+  //creating files inside src folder
+  await fs.mkdir(`${dirPath}/backend/controllers`, { recursive: true });
+  await fs.mkdir(`${dirPath}/backend/models`, { recursive: true });
+  await fs.mkdir(`${dirPath}/backend/db`, { recursive: true });
+  await fs.mkdir(`${dirPath}/backend/middlewares`, { recursive: true });
+  await fs.mkdir(`${dirPath}/backend/routes`, { recursive: true });
+  await fs.mkdir(`${dirPath}/backend/utils`, { recursive: true });
+
+  await fs.writeFile(`${dirPath}/backend/server.js`, "server");
+  await fs.writeFile(`${dirPath}/backend/.env`, "env");
+  await fs.writeFile(`${dirPath}/backend/db/index.js`, "database");
+
+}
+
 
 export async function createParentFolder(dirPath) {
   try {
